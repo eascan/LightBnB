e0f48232451a4f02bd494bac9e005a1367bdd25d
Edward <edward.ascan@me.com>
2021-02-04 03:21:44 +0000

    add all my reservations query

--- 1_queries/all_my_reservations.sql
+++ 1_queries/all_my_reservations.sql
@@ -1,1 +1,10 @@
-SELECT 
+SELECT reservations.*, properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN reservations ON properties.id = property_id
+JOIN property_reviews ON reservations.id = reservation_id
+JOIN users ON users.id = owner_id
+WHERE reservations.guest_id = 1 
+AND reservations.end_date < now()::date
+GROUP BY reservations.id, properties.id
+ORDER BY reservations.start_date
+LIMIT 10;
